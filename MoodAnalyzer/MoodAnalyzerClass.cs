9506729c64e5f42cba267cac8852d09a92b86767
vibha <vibhauc@gmail.com>
2022-03-06 21:29:17 +0530

    [Vibha]Added:To analyze and respond Happy or Sad mood

---
+++ MoodAnalyzer/MoodAnalyzerClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzer
+{
+    public class MoodAnalyzerClass
+    {      
+        //Method to analyse the mood from the given message
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "sad";
+            }
+            else
+            {
+                return "happy";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{    
+    public class Program
+    {        
+        public static void Main(string[] args)
+        {            
+            Console.WriteLine("Welcome To Mood Analyser Program");
+
+            //Calling the mood analyser object(UC1)
+            MoodAnalyzerClass mood = new MoodAnalyzerClass();
+            string result = mood.AnalyseMood("I am in sad mood");
+
+            Console.WriteLine("The mood is {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
