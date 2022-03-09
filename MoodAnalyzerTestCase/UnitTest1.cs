46b9dc6c074b0833af0e8e16ca050f912b821a32
vibha <vibhauc@gmail.com>
2022-03-09 22:45:19 +0530

    [Vibha]Added:Given Improper Constructor should throw Exception indicating no such method error

--- MoodAnalyzerTestCase/UnitTest1.cs
+++ MoodAnalyzerTestCase/UnitTest1.cs
@@ -6,6 +6,7 @@ namespace MoodAnalyzerTestCase
     [TestClass]
     public class UnitTest1
     {
+        //TC-1.1
         [TestMethod]
         public void GivenSadMessage_ReturnSadMood()
         {
@@ -21,6 +22,7 @@ namespace MoodAnalyzerTestCase
             Assert.AreEqual(expected, actual);
         }
 
+        //TC-1.2
         [TestMethod]
         public void GivenAnyMessage_ReturnHappyMood()
         {
@@ -53,6 +55,8 @@ namespace MoodAnalyzerTestCase
             Assert.AreEqual(expected, actual);
         }
 
+        //TC-3.1
+
         [TestMethod]
         public void GivenNullMood_ShouldReturn_NULL_MOOD_EXCEPTION()
         {
@@ -68,6 +72,8 @@ namespace MoodAnalyzerTestCase
             //Asert
             Assert.AreEqual(expected, actual);
         }
+
+        //TC-3.2
 
         [TestMethod]
         public void GivenEmptyMood_ShouldReturn_EMPTY_MOOD_EXCEPTION()
@@ -85,7 +91,7 @@ namespace MoodAnalyzerTestCase
             Assert.AreEqual(expected, actual);
         }
 
-
+        //Method to test Mood analyzer object UC4 - TC 4.1
         [TestMethod]
         public void Given_MoodAnalyser_ClassName_Should_Return_MoodAnalyser_Object1()
         {
@@ -99,6 +105,24 @@ namespace MoodAnalyzerTestCase
             //Assert
             expected.Equals(Obj);
         }
-        
+
+        [TestMethod]
+        public void Given_Improper_Class_Name_Should_Throw_MoodAnalysisException_Indicating_No_Such_Class()
+        {
+            try
+            {
+                //Arrange
+                string className = "WrongNamespace.MoodAnalyser";
+                string constructorName = "MoodAnalyser";
+                //Act
+                object resultObj = MoodAnalyserFactoryClass.CreateMoodAnalyserObject(className, constructorName);
+            }
+            catch (MoodAnalyzerCustomException e)
+            {
+                //Assert
+                Assert.AreEqual("class not found", e.Message);
+            }
+        }
+
     }
 }
